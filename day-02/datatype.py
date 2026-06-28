f84042306c3a7cef0b37bdab43d5ba82eac8878f
p manohar reddy <manoharreddyp61@gmail.com>
2026-06-28 22:13:47 +0530

    Add files via upload

---
+++ day-02/datatype.py
@@ -0,0 +1,11 @@
+a = 10 # 10 is an int datatype
+b = 5.0 # 5.0 float datatype
+c = 3j # 3j is an complex datatype
+d = True # boolean datatype
+e = "name" # string datatype
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ day-02/variables.py
@@ -0,0 +1,11 @@
+name ="p Manohar reddy"
+age = 24
+department = "CAI"
+percentage = "7.65"
+college ="siddharth institute of engineering and technology"
+ 
+ # here name,age,department,percentage,college are the variables
+""" 
+ and p Manohar reddy,24,CAI,7.65,siddharth 
+ institute of engineering and technology are key values that holdsby variable """
+print(name,age,department,percentage,college)
