4106c3fd841da01f46df8200d684cb3fd53cf3c3
p manohar reddy <manoharreddyp61@gmail.com>
2026-07-09 22:39:18 +0530

    Add files via upload

---
+++ day-04/arithmatic.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+print(a+b) #addition operator 
+print(a-b) #substraction operator
+print(a*b) #multiplication operator 
+print(a/b) # division operator 
+print(a % b) # modulous operator 
+print(a ** b) # exponent operator 
---
+++ day-04/boolean.py
@@ -0,0 +1,4 @@
+print(5 < 6) # the condition is True because 5 is less than 6 so it prints True value
+print(5 == 5) # the condition is True because 5 is equal to 5 so it prints True value
+print(5 != 6) # the condition is True because 5 is not equal to 6 so it prints True value
+print(5 > 6) # the condition is False because 5 is greater than 6 so it prints False value
