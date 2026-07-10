b2dfdded78ae6477c4bc21c3634b905b1156d72d
p manohar reddy <manoharreddyp61@gmail.com>
2026-07-10 23:01:07 +0530

    Add assignment operator examples in assignmentop.py

---
+++ day-04/assignmentop.py
@@ -0,0 +1,23 @@
+a = 15
+a+=15 # a= a+15
+print(a) # o/p:- 30
+
+a = 15
+a-=15 # a= a-15
+print(a)
+
+a = 15
+a*=15 # a= a*15
+print(a)
+
+a = 15
+a/=15 # a= a/15
+print(a)
+
+a = 15
+a%=15 # a= a%15
+print(a)
+
+a = 15
+a**=15 # a = a**15
+print(a)
