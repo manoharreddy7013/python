cdf24159c13edf8889593c9e7f295e1549635e76
p manohar reddy <manoharreddyp61@gmail.com>
2026-07-13 21:35:06 +0530

    Add files via upload

---
+++ day-07/elif.py
@@ -0,0 +1,9 @@
+x = 10
+if x > 10:
+    print(True)
+elif x >= 10:
+    print("x is greater than or equal to 10")
+elif x <= 10:
+     print("x is less than or equal to 10")
+else:
+    print(False)
---
+++ day-07/nestedif.py
@@ -0,0 +1,6 @@
+x = 15
+if x > 10: 
+   if x >= 15:
+       print("nested condition is true")
+else:
+    print("both the nested if condotons are false")
