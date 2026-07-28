6f776f9c77da751f31567cb720f7537539cf199d
p manohar reddy <manoharreddyp61@gmail.com>
2026-07-28 22:19:57 +0530

    Add files via upload

---
+++ day-15/iterator.py
@@ -0,0 +1,11 @@
+# bhav = ["manohar","mano","manu"]
+# na = iter(bhav)
+# print(next(na))
+# print(next(na))
+# print(next(na))
+
+
+#using for in terator 
+bhava = ["manohar","mano","manu"]
+for i in bhava:
+    print(i)
