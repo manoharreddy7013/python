5b5d5603c8ce6ebe9dfa35afc5b3724465c92efb
p manohar reddy <manoharreddyp61@gmail.com>
2026-07-31 23:34:38 +0530

    Add files via upload

---
+++ day-18/mathval.py
@@ -0,0 +1,28 @@
+# finding minimum value
+x = min(54,73,21)
+print(x)
+
+# finding maximum number
+y = max(54,73,21)
+print(y)
+
+# absolute value
+a = abs(-245)
+print(a)
+
+z = pow(2,7)
+print(a)
+
+# using math module 
+import math 
+b = math.sqrt(49)
+print(b)
+
+c = math.ceil(15.4)
+print(c)
+
+d = math.floor(15.4)
+print(d)
+
+e = math.pi
+print(e)
