f17327516c7eafd7b32ec1ef7a4690fc2eaf81c9
p manohar reddy <manoharreddyp61@gmail.com>
2026-08-06 21:40:30 +0530

    Add files via upload

---
+++ day-22/none,input.py
@@ -0,0 +1,15 @@
+x = None 
+print(x)
+print(type(x))
+
+result = None 
+if result is None:
+    print("the result is none")
+else:
+    print("the result is not null")
+
+# example code for user input 
+name = input("enter name")
+print(f"hello {name}")
+age = input("enter your age")
+print(f"hello {name} your age is {age}")
